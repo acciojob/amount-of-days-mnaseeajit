0962b0bab70d79698574b1c58e45b9cf4c5a99ee
mnaseeajit <147361506+mnaseeajit@users.noreply.github.com>
2023-12-08 14:03:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function output(yr) {
+    if ((yr % 4 == 0 && yr % 100 != 0) || (yr % 400 == 0)) {
+        return 366;
+    }
+    return 365;
+}
+
+let yr = prompt("daysOfAYear");
+alert(output(yr));
